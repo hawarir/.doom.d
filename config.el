2b893f7cac4cbb95cac08c42df419493aef4825b
Hawari Rahman <hawari.rahman17@gmail.com>
2019-10-05 14:38:21 +0700

    Add existing personal configuration

---
+++ config.el
@@ -0,0 +1,17 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+(require 'doom-themes)
+
+(load-theme 'doom-molokai)
+
+(defun my-go-mode-hook ()
+  ;; Modify go-mode indentation level
+  (setq tab-width 8))
+
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+(setq
+ js-indent-level 2
+ typescript-indent-level 2
+ json-reformat:indent-width 2
+ projectile-project-search-path '("~/work/projects" "~/personal/projects"))
