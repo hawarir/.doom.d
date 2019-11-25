74766a1d7da54646a785e1562f8e649aca9c9ec8
Hawari Rahman <hawari.rahman17@gmail.com>
2019-11-25 17:21:15 +0700

    Change several variables

--- config.el
+++ config.el
@@ -1,12 +1,13 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
-(defun my-go-mode-hook ()
-  ;; Modify go-mode indentation level
-  (setq tab-width 8))
-
-(add-hook 'go-mode-hook 'my-go-mode-hook)
 
 (setq
+ doom-font (font-spec :family "SF Mono" :size 12)
+ doom-themes-treemacs-theme "doom-colors"
+ web-mode-markup-indent-offset 2
+ web-mode-code-indent-offset 2
+ web-mode-css-indent-offset 2
  js-indent-level 2
  typescript-indent-level 2
  json-reformat:indent-width 2
+ css-indent-offset 2
  projectile-project-search-path '("~/work/projects" "~/personal/projects"))
