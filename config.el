5ac3af1882d7158e703b8c52d2e3881fea569a4f
Hawari Rahman <hawari.rahman17@gmail.com>
2020-10-21 13:28:48 +0700

    Update init.el list of available modules

--- config.el
+++ config.el
@@ -1,7 +1,7 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 (setq
- doom-font (font-spec :family "JetBrains Mono" :size 12)
+ doom-font (font-spec :family "JetBrains Mono" :size 13)
  doom-themes-treemacs-theme "doom-colors"
  web-mode-markup-indent-offset 2
  web-mode-code-indent-offset 2
@@ -10,4 +10,4 @@
  typescript-indent-level 2
  json-reformat:indent-width 2
  css-indent-offset 2
- projectile-project-search-path '("~/work/projects" "~/personal/projects"))
+ projectile-project-search-path '("~/code/"))
