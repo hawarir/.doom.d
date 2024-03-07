381295f89302dc3970d7b916a8aa57a732e0dff7
Hawari Rahman <hawari@xendit.co>
2024-03-07 14:29:01 +0700

    Tweak config

--- config.el
+++ config.el
@@ -5,13 +5,13 @@
 
 (setq
  doom-theme 'doom-tomorrow-night
- doom-font (font-spec :family "JetBrains Mono" :size 13)
+ doom-font (font-spec :family "JetBrains Mono" :size 12)
  doom-themes-treemacs-theme "doom-colors"
  org-priority-highest 65
  org-priority-lowest  68
  org-priority-default 66
  org-superstar-headline-bullets-list '("⁖" "◉" "○" "✸" "✿")
- projectile-project-search-path '("~/code"))
+ projectile-project-search-path '("~/code" "~/code/xendit"))
 
 (after! org-fancy-priorities
   (setq org-fancy-priorities-list '("⚡" "⬆" "⬇" "☕")))
